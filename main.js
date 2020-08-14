b04fb6aec0fa759eafafb630e337247b74016738
Jade-KR <kjd8765@gmail.com>
2020-08-14 22:25:04 +0900

    JS Home contact button

--- main.js
+++ main.js
@@ -3,8 +3,6 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY);
-  console.log(navbarHeight + "f")
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--dark');
   } else {
@@ -12,18 +10,26 @@ document.addEventListener('scroll', () => {
   }
 })
 
-// Hnadle scrolling when tapping on the navbar menu
+// Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', () => {
-  console.log(event.target.dataset.link);
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
     return;
   }
-  console.log(event.target.dataset.link)
-  const scrollTo = document.querySelector(link);
+  scrollIntoView(link);
+});
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+  scrollIntoView('#contact')
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({
     behavior: 'smooth'
-  })
-})
+  });
+}
