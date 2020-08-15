119e930c25cb8021fc2dce033d5b8aa28e64a2d7
Jade-KR <kjd8765@gmail.com>
2020-08-16 06:34:47 +0900

    JS All

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ document.addEventListener('scroll', () => {
   }
 })
 
+
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', () => {
@@ -18,8 +20,16 @@ navbarMenu.addEventListener('click', () => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
 });
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  console.log(navbarMenu)
+  navbarMenu.classList.toggle('open');
+})
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
@@ -49,7 +59,35 @@ arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
 })
 
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
 
+  // Remove selection from the previous item and select the new one
+  const active = document.querySelector('.category__btn.selected');
+  active.classList.remove('selected');
+  const target = e.target.nodeName === 'BUTTON' ? e.target :
+    e.target.parentNode;
+  target.classList.add('selected');
+
+  projectContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    projectContainer.classList.remove('anim-out');
+  }, 300);
+});
 
 
 
